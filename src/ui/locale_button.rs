c915395f29eba6cf355fe64dfe06ac593b84159f
kgv <kgv@users.noreply.github.com>
2025-02-17 17:27:18 +0300

    Enhance LocaleButton with size and text_style customization options

--- src/ui/locale_button.rs
+++ src/ui/locale_button.rs
@@ -1,28 +1,43 @@
 use crate::UiExt as _;
-use egui::{Response, RichText, Sense, Ui, Widget, WidgetText};
+use egui::{Response, RichText, Sense, TextStyle, Ui, Widget};
 use egui_phosphor::regular::TRANSLATE;
 
 /// Locale button
 pub struct LocaleButton {
-    title: Option<WidgetText>,
+    text: RichText,
 }
 
 impl LocaleButton {
     pub fn new() -> Self {
-        Self { title: None }
+        Self {
+            text: RichText::new(TRANSLATE),
+        }
+    }
+
+    /// See [`RichText::size`].
+    #[inline]
+    pub fn size(self, size: f32) -> Self {
+        Self {
+            text: self.text.size(size),
+        }
+    }
+
+    /// See [`RichText::text_style`].
+    #[inline]
+    pub fn text_style(self, text_style: TextStyle) -> Self {
+        Self {
+            text: self.text.text_style(text_style),
+        }
     }
 }
 
 impl Widget for LocaleButton {
     fn ui(self, ui: &mut Ui) -> Response {
-        let title = self
-            .title
-            .unwrap_or_else(|| RichText::new(TRANSLATE).into());
-        ui.menu_button(title, |ui| {
+        ui.menu_button(self.text, |ui| {
             let mut response = ui.allocate_response(Default::default(), Sense::click());
             let mut current_value = ui.language_identifier();
             for selected_value in ui.language_identifiers() {
-                let text = selected_value.to_string();
+                let text = RichText::new(selected_value.to_string()).heading();
                 response |= ui.selectable_value(&mut current_value, selected_value, text);
             }
             if response.changed() {
